539b9c650218b72608ba14899024cde5694c638b
lorenzo-lettieri <lorenzo-lettieri@lernende.bbw.ch>
2022-01-10 13:58:05 +0100

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
+import {Button, Card, Dropdown} from "react-bootstrap";
+import {useState} from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const months = ['January', 'February', 'March', 'April', 'June', 'July', 'August', 'November', 'December'];
+    const [currentMonth, setCurrentMoth] = useState("None");
+
+    return (
+        <div>
+            <div className='container-fluid'>
+                <h1>{currentMonth}</h1>
+                <Dropdown>
+                    <Dropdown.Toggle variant="success" id="dropdown-basic">
+                        Select month ...
+                    </Dropdown.Toggle>
+                    <Dropdown.Menu>
+                        {
+                            months.map((month) => {
+                                return <Dropdown.Item onClick={() => setCurrentMoth(month)}>{month}</Dropdown.Item>
+                            })
+                        }
+                    </Dropdown.Menu>
+                </Dropdown>
+
+                <Card style={{width: '18rem'}}>
+                    <Card.Body>
+                        <Card.Title>Card Title</Card.Title>
+                        <Card.Text>
+                            Some quick example text to build on the card title and make up the bulk of
+                            the card's content.
+                        </Card.Text>
+                        <Button variant="primary">Go somewhere</Button>
+                    </Card.Body>
+                </Card>
+            </div>
+        </div>
+    );
 }
 
 export default App;
