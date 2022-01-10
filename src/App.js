657467b4b366f06a60737a7d7ce522da11ffd083
lorenzo-lettieri <lorenzo-lettieri@lernende.bbw.ch>
2022-01-10 15:00:26 +0100

    renaming and refactoring

--- src/App.js
+++ src/App.js
@@ -1,41 +1,11 @@
 import './App.css';
-import {Button, Card, Dropdown} from "react-bootstrap";
-import {useState} from "react";
 
 function App() {
-    const months = ['January', 'February', 'March', 'April', 'June', 'July', 'August', 'November', 'December'];
-    const [currentMonth, setCurrentMoth] = useState("None");
-
-    return (
-        <div>
-            <div className='container-fluid'>
-                <h1>{currentMonth}</h1>
-                <Dropdown>
-                    <Dropdown.Toggle variant="success" id="dropdown-basic">
-                        Select month ...
-                    </Dropdown.Toggle>
-                    <Dropdown.Menu>
-                        {
-                            months.map((month) => {
-                                return <Dropdown.Item onClick={() => setCurrentMoth(month)}>{month}</Dropdown.Item>
-                            })
-                        }
-                    </Dropdown.Menu>
-                </Dropdown>
-
-                <Card style={{width: '18rem'}}>
-                    <Card.Body>
-                        <Card.Title>Card Title</Card.Title>
-                        <Card.Text>
-                            Some quick example text to build on the card title and make up the bulk of
-                            the card's content.
-                        </Card.Text>
-                        <Button variant="primary">Go somewhere</Button>
-                    </Card.Body>
-                </Card>
-            </div>
+    return (<div>
+        <div className='container-fluid'>
+            <h1>Hello World</h1>
         </div>
-    );
+    </div>);
 }
 
 export default App;
---
+++ src/service/newsService.js
@@ -0,0 +1,7 @@
+const url = process.env.REACT_APP_API_BASE_URL;
+
+export async function getPostByID(id) {
+}
+
+export async function addPost(month, rainfallObject) {
+}
