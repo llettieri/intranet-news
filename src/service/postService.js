4cae587e120da6b06e9dbd22cde87bd2d788f955
lorenzo-lettieri <lorenzo-lettieri@lernende.bbw.ch>
2022-01-17 16:47:13 +0100

    renaming and refactoring

---
+++ src/service/postService.js
@@ -0,0 +1,48 @@
+const url = process.env.REACT_APP_API_BASE_URL;
+
+//Create
+export async function addPost(post) {
+    return axios.post(url + 'post/', post)
+        .then((response) => {
+            return response;
+        })
+        .catch((error) => {
+            return error;
+        });
+}
+
+//Read
+export async function getPostByID(id) {
+}
+
+export async function getAllPosts() {
+    return axios.get(url + 'post/')
+        .then((response) => {
+            return response;
+        })
+        .catch((error) => {
+            return error;
+        });
+}
+
+//Update
+export async function updatePost(post) {
+    return axios.put(url + 'post/' + post.id, post)
+        .then((response) => {
+            return response;
+        })
+        .catch((error) => {
+            return error;
+        });
+}
+
+//Delete
+export async function deletePost(id) {
+    return axios.delete(url + 'post/' + id)
+        .then((response) => {
+            return response;
+        })
+        .catch((error) => {
+            return error;
+        });
+}
